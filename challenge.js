9bf2106806e03048153cd261f2c905690ffbc59a
MustapheMS <moztaphem9@gmail.com>
2023-05-22 19:31:15 +0300

    Football Points Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function FootballPoints(wins, draws, losses) {
+    const winPoints = wins * 3;
+    const drawPoints = draws * 1;
+    const totalPoints = winPoints + drawPoints;
+  
+    console.log(`The team has earned ${totalPoints} points.`);
+  }
+  FootballPoints(10, 2, 4);  
